851339bfd43e9b14bbcb12dbf7ad45303e000d02
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-28 10:24:37 -0300

    estilização e jsx da página Reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
+import "./style.css";
 
 // import { Container } from './styles';
 
 function Reservas() {
   return (
     <div>
-      <h1>Reservas</h1>
+      <h1 className="title">Você solicitou 1 Reserva</h1>
+      <div className="reservas">
+        <img />
+        <strong>Viagem Maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
     </div>
   );
 }

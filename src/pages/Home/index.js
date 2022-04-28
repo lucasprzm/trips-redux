aa66a9dc5c449d389ffd2b1de3704580f20cd1eb
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-28 10:24:04 -0300

    estilização e montagem página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
-
+import { MdFlightTakeoff } from "react-icons/md";
+import "./style.css";
 // import { Container } from './styles';
 
 function Home() {
@@ -15,7 +16,23 @@ function Home() {
   }, []);
   return (
     <div>
-      <h1>Home</h1>
+      <div className="box">
+        {trips.map((trip) => {
+          return (
+            <li key={trip.id}>
+              <img src={trip.image} alt={trip.title} />
+              <strong>{trip.title}</strong>
+              <span>Status: {trip.status ? "Disponível" : "Indisponível"}</span>
+              <button type="button" onClick={() => {}}>
+                <div>
+                  <MdFlightTakeoff size={16} color="#fff" />
+                </div>
+                <span>SOLICITAR RESERVA</span>
+              </button>
+            </li>
+          );
+        })}
+      </div>
     </div>
   );
 }

47eac1469f4eb1ebd1d044f5aee4c0bacfce1294
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-28 22:10:52 -0300

    ajuste dispatch do redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,18 +2,24 @@ import React, { useEffect, useState } from "react";
 import api from "../../services/api";
 import { MdFlightTakeoff } from "react-icons/md";
 import "./style.css";
-// import { Container } from './styles';
+import { useDispatch } from "react-redux";
+import { pushArray } from "../../store/modules/reserve/reducer";
 
 function Home() {
+  const dispatch = useDispatch();
   const [trips, setTrips] = useState([]);
   useEffect(() => {
     async function loadApi() {
       const response = await api.get("trips");
       setTrips(response.data);
-      console.log(response.data);
     }
     loadApi();
   }, []);
+
+  function handleAdd(trip) {
+    dispatch(pushArray(trip));
+    //  console.log(pushArray(trip));
+  }
   return (
     <div>
       <div className="box">
@@ -23,7 +29,7 @@ function Home() {
               <img src={trip.image} alt={trip.title} />
               <strong>{trip.title}</strong>
               <span>Status: {trip.status ? "Disponível" : "Indisponível"}</span>
-              <button type="button" onClick={() => {}}>
+              <button type="button" onClick={() => handleAdd(trip)}>
                 <div>
                   <MdFlightTakeoff size={16} color="#fff" />
                 </div>

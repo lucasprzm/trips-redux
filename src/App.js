4f72f423f0b1cd983ffca29409bdd7cc8bb419a5
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-29 17:12:03 -0300

    reversão do uso do history

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Routes from "./routes";
 import { Provider } from "react-redux";
+
 import store from "./store";
 
 function App() {

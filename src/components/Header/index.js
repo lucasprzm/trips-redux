5e58caba63fb0c5e3b91007fe4006552594a91ea
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-28 22:10:29 -0300

    função de número de reservas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import "./style.css";
-
-// import { Container } from './styles';
+import { useSelector } from "react-redux";
 
 function Header() {
+  const reserveSize = useSelector((state) => state.reserve.value);
+  // console.log(reserveSize);
   return (
     <header className="container">
       <Link to="/">
@@ -14,7 +15,7 @@ function Header() {
       <Link className="reserva" to="/reservas">
         <div>
           <strong>Minhas Reservas</strong>
-          <span>0 reservas</span>
+          <span>{reserveSize.length} reservas</span>
         </div>
       </Link>
     </header>
